f7395f59f9e9f2142b512fb30d24bdf804e3a9fa
Aradhya <2k23.cs2312107@gmail.com>
2024-09-28 00:02:52 +0530

    leetcode74

---
+++ LEETCODE_74_Search a 2D Matrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rowno= matrix.length;
+        int columnno = matrix[0].length;
+        int start = 0, end = rowno*columnno -1;
+        while (start <= end) {
+             int mid = start + (end - start) / 2;
+            int row= mid/columnno;
+            int column= mid%columnno;
+            if (matrix[row][column] == target) {
+                return true;
+            } else if (matrix[row][column] < target) {
+               start= mid+1;
+            } else {
+                end=mid-1;
+                
+            }
+        }
+        return false;
+
+    }
+}
