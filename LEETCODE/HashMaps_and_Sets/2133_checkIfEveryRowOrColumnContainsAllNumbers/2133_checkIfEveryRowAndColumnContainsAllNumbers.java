0f38f2a11c78bc04455c754947c1cc9bb0cb7ae7
Aradhya <2k23.cs2312107@gmail.com>
2025-01-23 22:28:57 +0530

    GOOGLE Easy

---
+++ LEETCODE/HashMaps_and_Sets/2133_checkIfEveryRowOrColumnContainsAllNumbers/2133_checkIfEveryRowAndColumnContainsAllNumbers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkValid(int[][] matrix) {
+        int n= matrix.length;
+        for(int i=0;i<n;i++){
+            HashSet<Integer> set= new HashSet<>();
+            for(int j=0;j<n;j++){
+                if(set.contains(matrix[i][j]))
+                    return false;
+                set.add(matrix[i][j]);
+            }
+        }
+
+         for(int i=0;i<n;i++){
+            HashSet<Integer> set= new HashSet<>();
+            for(int j=0;j<n;j++){
+                if(set.contains(matrix[j][i]))
+                    return false;
+                set.add(matrix[j][i]);
+            }
+        }
+        return true;
+
+        
+    }
+}
