6401a40e33d8fd551efbf0f871ba560914e107c6
Aradhya <2k23.cs2312107@gmail.com>
2025-04-10 11:22:56 +0530

    added Longest Common Substring Code as well

--- LEETCODE/DynamicProgramming/Strings/1143_longestCommonSubsequence/1143_longestCommonSubsequence.java
+++ LEETCODE/DynamicProgramming/Strings/1143_longestCommonSubsequence/1143_longestCommonSubsequence.java
@@ -168,3 +168,36 @@ class Solution {
         }
     }
 }
+
+
+//for longest common substring
+
+class Solution {
+    public int longestCommonSubstr(String s1, String s2) {
+        // code here
+        int n= s1.length(),m=s2.length();
+        int dp[][]=new int[n+1][m+1];
+        //  if(i==0 || j==0) return 0;
+        for(int j=0;j<m;j++) dp[0][j]=0;
+        for(int i=0;i<n;i++) dp[i][0]=0;
+        /*if(s1[i-1]==s2[j-1]) return dp[i][j]=1+solve(i-1,j-1,s1,s2,dp);
+        else
+            return dp[i][j]=Math.max(solve(i-1,j,s1,s2,dp),solve(i,j-1,s1,s2,dp)); */
+            
+        int maxi=Integer.MIN_VALUE;
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1 + dp[i-1][j-1];
+                    maxi= Math.max(maxi,dp[i][j]);
+                }
+                else{
+                    
+                    // dp[i][j]= Math.max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return Math.max(maxi,0);
+    }
+}
